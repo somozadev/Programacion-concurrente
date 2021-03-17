1550cdb3139c68deec40f09284084f22cc76e90c
somozadev <somozadev@gmail.com>
2021-03-17 17:23:03 +0100

    Finished classwork1st

---
+++ CHAT/cliente.py
@@ -0,0 +1,46 @@
+import threading
+import sys
+import socket
+import pickle
+import os
+
+class Cliente():
+
+	# def __init__(self, host = '10.34.84.147', port=61000):
+	def __init__(self, host=socket.gethostname(), port=59989):
+		self.sock = socket.socket()
+		username = input('Username: ')
+		self.sock.connect((str(host), int(port)))
+		hilo_recv_mensaje = threading.Thread(target=self.recibir)
+		hilo_recv_mensaje.daemon = True
+		hilo_recv_mensaje.start()
+		print('Hilo con PID',os.getpid())
+		print('Hilos activos', threading.active_count())
+		self.enviar(username + " se ha conectado")
+		while True:
+			# msg = input('\nEscriba texto ? ** Enviar = ENTER ** Abandonar Chat = Q \n')
+			msg = input("\n>>\n")
+			if msg != 'Q' :
+				self.enviar(username + ": " + msg)
+			else:
+				print(" ****LOGOUT****")
+				self.sock.close()
+				sys.exit()
+
+	def recibir(self):
+		while True:
+			try:
+				data = self.sock.recv(1024)
+				if data:
+					print(pickle.loads(data))
+			except:
+				pass
+
+	def enviar(self, msg):
+		self.sock.send(pickle.dumps(msg))
+		data = pickle.dumps(msg)
+		if data: 
+			print(pickle.loads(data))
+c = Cliente()
+
+		
